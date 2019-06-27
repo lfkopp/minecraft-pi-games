d3096ebcf3590b96df6c213c4d72f64cf02f437d
lfkopp <lfkopp@gmail.com>
2019-06-27 16:04:32 -0300

    hit

--- mapa_de_bloco.py
+++ mapa_de_bloco.py
@@ -6,16 +6,11 @@ from time import sleep
 mc = minecraft.Minecraft.create()
 
 while True:
-    #Get the block hit events
-    sleep(3)
-    blockHits = mc.events.pollBlockHits()
-    # if a block has been hit
-    if blockHits:
-        print("q")
-        # for each block that has been hit
-        for blockHit in blockHits:
-            # do something with the block
-            print(blockHit.pos.x,blockHit.pos.y,blockHit.pos.z)
-            print(blockHit.face)
-            print(blockHit.type)
-            print(blockHit.entityId)
+    hits = mc.events.pollBlockHits()
+    for hit in hits:
+        print(mc.getBlockWithData(hit.pos.x,hit.pos.y,hit.pos.z).id)
+        print(mc.getBlockWithData(hit.pos.x,hit.pos.y,hit.pos.z).data)
+        print(mc.getBlockWithData(hit.pos.x,hit.pos.y,hit.pos.z).withData.__str__)
+        mc.setBlocks(hit.pos.x-1,hit.pos.y-1,hit.pos.z-1,
+                     hit.pos.x+1,hit.pos.y+1,hit.pos.z+1,0)
+        
