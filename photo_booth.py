2f65aa84764fa31ec75ec4fe9a3a007dd40e30df
labnet kopp <suporte@labnet.nce.ufrj.br>
2019-06-27 14:09:22 -0300

    utf

--- photo_booth.py
+++ photo_booth.py
@@ -1,4 +1,4 @@
-from PIL import Image
+ï»¿from PIL import Image
 from mcpi import minecraft, block
 
 mc = minecraft.Minecraft.create()
@@ -6,16 +6,13 @@ mc.postToChat('Sou tricolor de coracao!!')
 
 player_pos = mc.player.getPos()
 
-im = Image.open('flu.png')
-pixels = im.load()
 img = Image.open('flu.png')
 baseheight = 40
 hpercent = (baseheight / float(img.size[1]))
 wsize = int((float(img.size[0]) * float(hpercent)))
 img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
-img.save(‘resized_img.bmp')
-
-img = Image.open(‘resized_img.bmp')
+img.save('resized_img.bmp')
+img = Image.open('resized_img.bmp')
 pixels = img.load()
 
 
