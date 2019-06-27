793ea55acab5a42707355fadc23e931db8becddd
labnet kopp <suporte@labnet.nce.ufrj.br>
2019-06-27 14:01:38 -0300

    imag

--- photo_booth.py
+++ photo_booth.py
@@ -6,13 +6,20 @@ mc.postToChat('Sou tricolor de coracao!!')
 
 player_pos = mc.player.getPos()
 
-im = Image.open('flu.png')
-pixels = im.load()
+img = Image.open('flu.png')
+baseheight = 40
+hpercent = (baseheight / float(img.size[1]))
+wsize = int((float(img.size[0]) * float(hpercent)))
+img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+img.save(‘resized_img.bmp')
+
+img = Image.open(‘resized_img.bmp')
+pixels = img.load()
 
 for i in range(im.size[0]):
     for j in range(im.size[1]):
         pixel = pixels[i,j]
-		mc.postToChat(str(pixel))
+        mc.postToChat(str(pixel))
         if pixel[0] > 100:
             type = block.STONE
         else:
