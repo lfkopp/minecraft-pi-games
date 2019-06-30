a80d8968f3071368f11a36da67685ff79ce8023a
lfkopp <lfkopp@gmail.com>
2019-06-30 15:58:09 -0300

    goleiro

--- futebol.py
+++ futebol.py
@@ -1,6 +1,11 @@
 from mcpi.minecraft import Minecraft
 from time import sleep
 from random import randrange
+import pygame
+
+pygame.mixer.init()
+
+
 
 mc = Minecraft.create()
 x, y, z = mc.player.getPos()
@@ -13,7 +18,9 @@ cores = {'white':0,'orange':1,'magenta':2,'cyan':3,'yellow':4,'green':5,
 
 
 def foi_gol(x,z,fla,flu):
-    if (-5 < x < 5) and (z < -31):
+    if (-5 <= x <= 5) and (z < -31):
+        pygame.mixer.music.load("wav/gol_fla.wav")
+        pygame.mixer.music.play()
         fla += 1
         mc.postToChat("ahhhh gol do Flamengo")
         sleep(3)
@@ -22,7 +29,12 @@ def foi_gol(x,z,fla,flu):
         mc.setBlock(x, 1, z, 0) #limpa bola
         mc.setBlock(0, 1, 0, 41) #bola
         return 0,0,fla,flu
-    elif (-5 < x < 5) and (z > 31):
+    elif (-5 <= x <= 5) and (z > 31):
+        if 15 < randrange(0,20) < 18:
+            pygame.mixer.music.load("wav/gol_ganso.wav")
+        else:
+            pygame.mixer.music.load("wav/gol_flu.wav")
+        pygame.mixer.music.play()
         flu +=1
         mc.postToChat("gol do Fluminense")
         sleep(3)
@@ -68,14 +80,25 @@ def tem_espaco(x,z):
 def chuta_bola(x,z):
     hits = mc.events.pollBlockHits()
     for hit in hits:
+        if 15 < randrange(0,20) < 18:
+            pygame.mixer.music.load("wav/emocionante.wav")
+        elif 10 < randrange(0,20) < 15:
+            pygame.mixer.music.load("wav/toque.wav")
+        else:
+            pygame.mixer.music.load("wav/chute.wav")
+        pygame.mixer.music.play()
         print(hit.face, mc.getBlockWithData(hit.pos.x,hit.pos.y,hit.pos.z).id)
         if mc.getBlockWithData(hit.pos.x,hit.pos.y,hit.pos.z).id == bola:
+            jx,jy,jz = mc.player.getPos()
+            deltax = (hit.pos.x-jx)/3
             for t in range(randrange(5,10)):
                 oldx,oldz = hit.pos.x,hit.pos.z
-                if hit.face == 2 and tem_espaco(hit.pos.x,hit.pos.z+1):
+                if hit.face == 2 and tem_espaco(hit.pos.x+ deltax,hit.pos.z+1):
                     hit.pos.z = hit.pos.z +1
-                elif hit.face == 3 and tem_espaco(hit.pos.x,hit.pos.z-1):
+                    hit.pos.x = hit.pos.x + deltax
+                elif hit.face == 3 and tem_espaco(hit.pos.x+ deltax,hit.pos.z-1):
                     hit.pos.z = hit.pos.z-1
+                    hit.pos.x = hit.pos.x + deltax
                 elif hit.face == 4 and tem_espaco(hit.pos.x+1,hit.pos.z):
                     hit.pos.x = hit.pos.x+1
                 elif hit.face == 5 and tem_espaco(hit.pos.x-1,hit.pos.z):
